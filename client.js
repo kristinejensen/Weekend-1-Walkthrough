48680fcd2e3f6f1e36357dcc7a997bc8f9199a9f
Kristine Jensen <kristinejensen@Kristines-MacBook-Pro.local>
2017-02-13 10:38:14 -0600

    Completed Base Mode

--- client.js
+++ client.js
@@ -1,5 +1,5 @@
-$(document).ready(function(){
-  $('#submitNewEmployee').on('click', function() { // Button is not created dynamically, so it doesn't need a specifier.
+$(document).ready(function(){ // Waits for the DOM to be completely loaded
+  $('#submitNewEmployee').on('click', function() { // Event listener -- listening for a click. Button is not created dynamically, so it doesn't need a specifier.
 
   // Declaring variables and retrieving values from input boxes
   var firstName = $('#firstName').val();
@@ -16,5 +16,14 @@ $(document).ready(function(){
   '<td>' + jobTitle +'</td>' +
   '<td>' + annualSalary + '</td>' +
   '</tr>'); // .append expects a string.
+
+// Add monthly salary expenses to the DOM
+var newEmployeeMonthlyExpenses = annualSalary / 12; // Takes annual salary and divide by 12. Division uses type coercion.
+var previousSalaryTotal = $('#monthlyExpenses').text(); // This gets the currently monthly expenses from the DOM. ".text" is used for different elements, not input box values. ".text" with empty parameters grabs the value of that text.
+var totalMonthlyExpenses = parseFloat(previousSalaryTotal) + newEmployeeMonthlyExpenses; // parseFloat turns a string into a number.
+$('#monthlyExpenses').text(totalMonthlyExpenses); // if you pass something into .text, you are replacing that item with the variable value
+
 });
 });
+
+//If things aren't working, test with console log. 
